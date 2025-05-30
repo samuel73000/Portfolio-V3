a110b5bc5d534dbe9a26ebe4c162625bfb033be8
Samuel Pouard <50187969+samuel73000@users.noreply.github.com>
2025-05-30 17:01:14 +0200

    portfolio-v3

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
